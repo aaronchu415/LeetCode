42b36de101ebb6969d2358bdee0aab271766f693
aaronchu415 <aaronchu415@gmail.com>
2018-12-25 14:49:01 -0800

    second

--- completed/leetcode-JewelsStone.js
+++ completed/leetcode-JewelsStone.js
@@ -2,9 +2,21 @@ let J = "aA"
 let S = "aAAbbbb"
 
 /**
+ Link: https://leetcode.com/problems/jewels-and-stones/
+
+You're given strings J representing the types of stones that are jewels,
+and S representing the stones you have.
+Each character in S is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
 Input: J = "aA", S = "aAAbbbb"
 Output: 3
+
+Input: J = "z", S = "ZZ"
+Output: 0
 
  * @param {string} J
  * @param {string} S
@@ -15,8 +27,10 @@ var numJewelsInStones = function (J, S) {
 
     let count = 0;
 
+    //loop through each "stone" in S
     for (let i = 0; i < S.length; i++) {
         let currChar = S[i];
+        //if J includes the current char then add to count
         if (currChar, J.includes(currChar)) {
             count++;
         }
--- completed/leetcode-findDisappearedNum.js
+++ completed/leetcode-findDisappearedNum.js
@@ -1,4 +1,19 @@
 /**
+ Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+ Solution:
+ Runtime - O(N)
+ Space - O(N)
+
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array),
+some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime?
+You may assume the returned list does not count as extra space.
+
+
  * @param {number[]} nums
  * @return {number[]}
  */
@@ -13,18 +28,22 @@ let input = [
     3,
     1
 ];
+
 var findDisappearedNumbers = function (nums) {
 
     let missing = []
     let map = {}
 
+    //for each number, add unique to map
     nums.forEach((n, i) => {
         if (map[n] === undefined) {
             map[n] = "F";
         }
     })
 
+    //for each num from i to N
     for (let i = 1; i <= nums.length; i++) {
+        //if num is not in map, it is a missing num
         if (map[i] === undefined) {
             missing.push(i);
         }
