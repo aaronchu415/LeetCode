db09a5c6be3e4f4ce77f9f7bcfd67969eea44933
aaronchu415 <aaronchu415@gmail.com>
2019-07-24 17:36:44 -0700

    more sol

--- completed/codeSignal-DropBoxPasswords.js
+++ completed/codeSignal-DropBoxPasswords.js
@@ -7,7 +7,6 @@
 // Opening the Dropbox mobile app on the VIU's tablet requires a four-digit passcode. To ensure the confidentiality of the stored information, the device is locked out of Dropbox after 10 consecutive failed passcode attempts. We need to implement a function that given an array of attempts made throughout the day and the correct passcode checks to see if the device should be locked, i.e. 10 or more consecutive failed passcode attempts were made.
 
 
-
 function incorrectPasscodeAttempts(passcode, attempts) {
 
   let correct_pass = false
---
+++ completed/epi-8:8.py
@@ -0,0 +1,39 @@
+# // _NAME: Implement a queue using stacks
+# // _LINK: https://elementsofprogramminginterviews.com/
+# // _CATEGORY: EPI
+
+
+class Queue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enq(self, x):
+        self.stack1.append(x)
+
+    def deq(self):
+        if len(self.stack1) == 0:
+            return None
+
+        while len(self.stack1) > 1:
+            self.stack2.append(self.stack1.pop())
+
+        n = self.stack1.pop()
+
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+        return n
+
+
+q1 = Queue()
+
+q1.enq(1)
+q1.enq(2)
+q1.enq(3)
+
+print(q1.deq())
+print(q1.deq())
+print(q1.deq())
+print(q1.deq())
+print(q1.deq())
